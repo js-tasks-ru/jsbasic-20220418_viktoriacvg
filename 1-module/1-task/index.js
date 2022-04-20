babe07b1d1f1e265a58927341afd6724b1e99860
Viktoria Klimahovich <viktoriacvg@gmail.com>
2022-04-20 19:39:47 +0300

    вычисление суммы и вычисление факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
-function factorial(n) {
-  // ваш код...
+function factorial (n) {
+  let result = 1; 
+  while (n) {
+     console.log (`факториал от ${n}`)
+      result=result*(n--);
 }
+  console.log(result);
+  return result;
+}
+alert(factorial (5));
